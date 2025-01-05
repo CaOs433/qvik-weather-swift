f3f410961838aa931005944ece3324de79181b9e
Oskari Saarinen <oskari.saarinen@saario.xyz>
2025-01-05 19:11:05 +0200

    Initial Commit

---
+++ Qvik-Weather-Swift/Qvik_Weather_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qvik_Weather_SwiftApp.swift
+//  Qvik-Weather-Swift
+//
+//  Created by Oskari Saarinen on 5.1.2025.
+//
+
+import SwiftUI
+
+@main
+struct Qvik_Weather_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Qvik-Weather-SwiftTests/Qvik_Weather_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Qvik_Weather_SwiftTests.swift
+//  Qvik-Weather-SwiftTests
+//
+//  Created by Oskari Saarinen on 5.1.2025.
+//
+
+import Testing
+@testable import Qvik_Weather_Swift
+
+struct Qvik_Weather_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
